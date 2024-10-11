c338c632985f306659565bafaf0fecb79ef6e700
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-10-11 20:14:15 +0200

    fix: PEP8 validations

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -6,7 +6,8 @@ function to return pascal triangle
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal's triangle of n.
+    Returns a list of lists of integers representing the Pascal's
+    triangle of n.
 
     Args:
         n (int): The number of rows to generate in Pascal's triangle.
